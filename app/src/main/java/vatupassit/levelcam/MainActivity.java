b21d1a9e07caa58176427682985cdc0d82895527
Joni Juntunen <Joni.M.Juntunen@student.lamk.fi>
2018-11-19 09:10:41 +0200

    init
    Android Studio v. 3.2.1
    Gradle v. 4.6

---
+++ app/src/main/java/vatupassit/levelcam/MainActivity.java
@@ -0,0 +1,20 @@
+package vatupassit.levelcam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import vatupassit.levelcam.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow();
+        }
+    }
+}
